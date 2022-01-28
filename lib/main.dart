314478f1194826dba8d83abc58127be66dcf99bc
Wenbiao Chen <2148244129@qq.com>
2022-01-28 11:44:05 +0800

    Add windows support

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ final GlobalKey<NavigatorState> navigatorKey =
 // }
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
   if (Platform.isAndroid) {
 //     // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
     SystemUiOverlayStyle systemUiOverlayStyle =
@@ -189,7 +189,6 @@ class Weiget_1 extends StatefulWidget {
 class _Weiget_1 extends State<Weiget_1> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     throw UnimplementedError();
   }
 }
